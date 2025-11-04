f9311d0e069d9060244042f0d3c4754657a2028f
Mark Dolmayeh <markdolmayehdev@gmail.com>
2025-11-04 21:49:21 +0200

    set up core of expr crate – tokenizer done, parser + AST mostly there (eval next)

---
+++ expr/target/debug/deps/expr-6e63bd68d610f6b8.d
@@ -0,0 +1,12 @@
+C:\Users\markd\Desktop\Scripture\expr\target\debug\deps\expr-6e63bd68d610f6b8.d: src\lib.rs src\token.rs src\ast.rs src\parser.rs src\eval.rs src\error.rs
+
+C:\Users\markd\Desktop\Scripture\expr\target\debug\deps\libexpr-6e63bd68d610f6b8.rlib: src\lib.rs src\token.rs src\ast.rs src\parser.rs src\eval.rs src\error.rs
+
+C:\Users\markd\Desktop\Scripture\expr\target\debug\deps\libexpr-6e63bd68d610f6b8.rmeta: src\lib.rs src\token.rs src\ast.rs src\parser.rs src\eval.rs src\error.rs
+
+src\lib.rs:
+src\token.rs:
+src\ast.rs:
+src\parser.rs:
+src\eval.rs:
+src\error.rs:
